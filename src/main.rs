c2fc04d459cf704dd4bc5c418a791022f3be8bfc
yosaka <fallingfluit@gmail.com>
2023-11-01 03:26:41 +0900

    now template

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,32 @@
 #![allow(clippy::comparison_chain)]
 #![allow(clippy::nonminimal_bool)]
 #![allow(clippy::neg_multiply)]
-#![allow(dead_code)]
+use amplify::confinement::Collection;
+use lazy_static::lazy_static;
+use num_bigint::BigInt;
 use std::collections::{BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 use std::f64::consts::PI;
+use std::hash::Hash;
 use std::mem::swap;
+use std::ops::Index;
+use std::ops::Mul;
 
+use itertools::{CombinationsWithReplacement, Itertools};
+use libm::{ceil, log};
 use num::{integer::Roots, traits::Pow, ToPrimitive};
-use proconio::input;
+use num_integer::{div_ceil, Integer};
+use num_traits::{abs_sub, AsPrimitive, Float};
+use permutohedron::{Heap, LexicalPermutation};
 use proconio::marker::{Chars, Usize1};
-use whiteread::parse_line;
-// use std::io::*;
-fn solve() {}
+use proconio::{input, source::line::LineSource};
+use std::convert::From;
+use std::{io::*, string};
+
+fn solve() {
+    #[rustfmt::skip]
+    input! {
+    }
+}
 
 fn main() {
     // input! {N:usize}
