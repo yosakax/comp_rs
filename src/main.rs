924e5c0f1178a5a7889db88a94e7b93badccae79
yosaka <fallingfluit@gmail.com>
2024-02-18 01:19:18 +0900

    add ac_library crates

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,23 @@
 #![allow(clippy::comparison_chain)]
 #![allow(clippy::nonminimal_bool)]
 #![allow(clippy::neg_multiply)]
+use ac_library::*;
 use amplify::confinement::Collection;
+use itertools::{CombinationsWithReplacement, Itertools};
 use lazy_static::lazy_static;
+use libm::{ceil, log};
+use num::{integer::Roots, traits::Pow, ToPrimitive};
 use num_bigint::BigInt;
+use num_integer::{div_ceil, Integer};
+use proconio::marker::{Chars, Usize1};
+use proconio::{input, source::line::LineSource};
 use std::collections::{BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+use std::convert::From;
 use std::f64::consts::PI;
 use std::hash::Hash;
 use std::mem::swap;
 use std::ops::Index;
 use std::ops::Mul;
-
-use itertools::{CombinationsWithReplacement, Itertools};
-use libm::{ceil, log};
-use num::{integer::Roots, traits::Pow, ToPrimitive};
-use num_integer::{div_ceil, Integer};
-use num_traits::{abs_sub, AsPrimitive, Float};
-use permutohedron::{Heap, LexicalPermutation};
-use proconio::marker::{Chars, Usize1};
-use proconio::{input, source::line::LineSource};
-use std::convert::From;
 use std::{io::*, string};
 
 fn solve() {
